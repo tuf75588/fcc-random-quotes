f1c4c0108428edaba1813df2e44e86797bad1e4b
Andrew Davis <atd285@gmail.com>
2019-01-20 14:15:04 -0500

    finish

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 console.log('connected!');
 const quoteSpan = document.querySelector('#text');
-const authorSpan = document.querySelector('.author');
-const btn = document.querySelector('.btn');
-const h1 = document.createElement('h1');
+const authorSpan = document.querySelector('#author');
+const btn = document.querySelector('#new-quote');
+const h2 = document.createElement('h2');
 const h3 = document.createElement('h3');
 const endpoint =
   'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
+document.addEventListener('DOMContentLoaded', addQuoteToDOM);
 
 async function fetchRandomQuotes() {
   let request = await fetch(endpoint);
@@ -21,9 +23,10 @@ function getRandomQuote() {
 }
 function addQuoteToDOM() {
   return getRandomQuote().then(({ quote, author }) => {
-    h1.textContent = quote;
+    h2.textContent = quote;
     h3.textContent = `Author: ${author}`;
-    quoteSpan.appendChild(h1);
+    h2.style.textAlign = 'center';
+    quoteSpan.appendChild(h2);
     authorSpan.appendChild(h3);
   });
 }
